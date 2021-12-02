7ee5ea2d9e7d53d38411fe1622f9aaeb5cd30b75
Lionel Morgan <lionelmorgan09@gmail.com>
2021-12-02 12:57:27 -0800

    Add files via upload

---
+++ src/main/java/util/test/java/dao/ClientsDaoImplTest.java
@@ -0,0 +1,96 @@
+package dao;
+import models.Clients;
+import org.junit.jupiter.api.*;
+import services.ClientsService;
+import util.H2Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import dao.ClientsDao;
+import dao.ClientsDaoImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ClientsDaoImplTest {
+
+
+
+        ClientsDao clientsDao;
+
+        public ClientsDaoImplTest(){
+            this.clientsDao = new ClientsDaoImpl(H2Util.url,H2Util.username, H2Util.password);
+        }
+
+        @BeforeEach
+        void setUp() {
+            H2Util.createTable();
+        }
+
+        @AfterEach
+        void tearDown() {
+            H2Util.dropTable();
+        }
+
+        @BeforeAll
+        static void beforeAll(){
+            System.out.println("This will run before all tests");
+
+        }
+        @BeforeEach
+        void beforeEach(){
+            System.out.println("This will run before each test");
+        }
+
+        @Test
+        void getAllClientsIT(){
+                //arrange
+                List<Clients> expectedResult = new ArrayList<>();
+                expectedResult.add(new Clients(1, "alennox@dreamville.com", "Ari", "Lennox"));
+                expectedResult.add(new Clients(2, "cbailey24@yahoo.com", "Champ", "Bailey"));
+                expectedResult.add(new Clients(3, "dnowitzki@dallasmavs.net", "Dirk", "Nowitzki"));
+                expectedResult.add(new Clients(4, "emusk@spacex.com", "Elon", "Musk"));
+                expectedResult.add(new Clients(5, "jbrown@soulmail.com", "James", "Brown"));
+                expectedResult.add(new Clients(6, "jcole@dreamville.com", "Jermaine", "Cole"));
+                expectedResult.add(new Clients(7, "ljames@lalakers.com", "Lebron", "James"));
+                expectedResult.add(new Clients(8, "lrichie@outlook.com", "Lionel", "Richie"));
+                expectedResult.add(new Clients(9, "mjackson@astromail.com", "Michael", "Jackson"));
+                expectedResult.add(new Clients(10, "mjordan@spacemail.net", "Michael", "Jordan"));
+                expectedResult.add(new Clients(11, "nosaka@olympics.com", "Naomi", "Osaka"));
+                expectedResult.add(new Clients(12, "obrown@thatsoraven.net", "Orlando", "Brown"));
+                expectedResult.add(new Clients(13, "pgeorge@clippers.com", "Paul", "George"));
+                expectedResult.add(new Clients(14, "eqelizabeth@england.eng", "Queen", "Elizabeth"));
+                expectedResult.add(new Clients(15, "rmoss@patriots.com", "Randy", "Moss"));
+                expectedResult.add(new Clients(16, "kbryant@lalakers.com", "Kobe", "Bryant"));
+
+                clientsDao.createClient(expectedResult.get(0));
+                clientsDao.createClient(expectedResult.get(1));
+                clientsDao.createClient(expectedResult.get(2));
+                clientsDao.createClient(expectedResult.get(3));
+                clientsDao.createClient(expectedResult.get(4));
+                clientsDao.createClient(expectedResult.get(5));
+                clientsDao.createClient(expectedResult.get(6));
+                clientsDao.createClient(expectedResult.get(7));
+                clientsDao.createClient(expectedResult.get(8));
+                clientsDao.createClient(expectedResult.get(9));
+                clientsDao.createClient(expectedResult.get(10));
+                clientsDao.createClient(expectedResult.get(11));
+                clientsDao.createClient(expectedResult.get(12));
+                clientsDao.createClient(expectedResult.get(13));
+                clientsDao.createClient(expectedResult.get(14));
+                clientsDao.createClient(expectedResult.get(15));
+
+                //act
+                List<Clients> actualResult = clientsDao.getAllClients();
+
+
+                //assert
+                assertEquals(expectedResult.toString(), actualResult.toString());
+            }
+
+        }
+
+
